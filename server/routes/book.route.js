98fb4f3785ab39fe00ec85499c3e226c50497ca0
charu <charupsharma5@gmail.com>
2024-06-28 22:06:35 +0530

    route created

--- server/routes/book.route.js
+++ server/routes/book.route.js
@@ -1,14 +1,41 @@
 import express from 'express';
-import route from express.route;
+import route from express.Router;
 import Book from '../models/book.model.js';
 
 const router = route();
 
-router.get("/", async (req, res) => {
+//Get all books
+router.get("/books", async (req, res) => {
     try {
         const books = await Book.find();
         res.json(books);
     } catch (err) {
         res.status(500).json({ message: err.message });
+    }
+});
+
+//Add a new book
+router.post("/books", async (req, res) => {
+    const book = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        year: req.body.year
+    })
+    try {
+        const newBook = await book.save();
+        res.status(200).json(newBook);
+    } catch (error) {
+        req.status(400).json({ message: error.message });
+    }
+});
+
+//To search for books
+router.get("/books/search", async (req, res) => {
+    const { title } = req.query
+    try {
+        const books = await Book.find({ title: newReg(title, "i") })
+        res.json(books);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 })
